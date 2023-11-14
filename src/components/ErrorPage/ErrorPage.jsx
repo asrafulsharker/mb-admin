034c17cd53133f5c2e4f9d09578e41779855dfba
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2023-11-14 15:13:09 +0600

    add

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -6,8 +6,8 @@ function ErrorPage() {
  
 
   return (
-    <div className='flex' style={{display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column"}}>
-        <h1>404 not found</h1>
+    <div className='flex' style={{display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column",height:"100vh"}}>
+        <h1 style={{fontSize:"50px"}}>404 not found</h1>
         <Link className='btn btn-primary' to='/'>
         Back to Home
       </Link>
